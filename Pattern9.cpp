78d5445fd039fba0d0defa286b1300906364ea2f
Priyanshu kumar <priyanshukumar23933@gmail.com>
2024-05-14 12:33:09 +0000

    updated

---
+++ Pattern9.cpp
@@ -0,0 +1,57 @@
+/*
+    *      *
+    **    **
+    ***  ***
+    ********
+    ********
+    ***  ***
+    **    **
+    *      *
+
+ Butterfly Pattern
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 10;
+    // Upper half
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
+        for (int j = 1; j <= 2 * (n - i); j++)
+        {
+            cout << " ";
+        }
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    // Lower Half
+
+    for (int i = n; i >= 1; i--)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
+        for (int j = 1; j <= 2 * (n - i); j++)
+        {
+            // for spaces
+            cout << " ";
+        }
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
