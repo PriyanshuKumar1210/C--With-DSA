8505af3626292d87546c3df95142ef71ce976b3b
Priyanshu kumar <priyanshukumar23933@gmail.com>
2024-05-19 20:59:43 +0000

    updated

--- Array/MaxSumSubArray.cpp
+++ Array/MaxSumSubArray.cpp
@@ -17,27 +17,39 @@
 */
 
 #include <iostream>
+#include <limits.h>
 using namespace std;
 
 void sumSubArray(int *arr,int n){
+
+    int maxsum = INT_MIN; //INT_MIN is a macro which is defined the -∞ (infinity)
     for(int start=0;start<n;start++){
         
         for(int end=start;end<=n-1;end++){
             int currsum = 0;
             for(int i=start;i<=end;i++){
                 currsum += arr[i];
-          //      cout<<arr[i];
+          
             }
             
-         //  cout<<",";
-          cout<<currsum<<",";
+           maxsum = max(maxsum,currsum);  //max is predefined function which is used to find the maximum no.b/w them
+            
+// Find the maximum using if-else condition 
+
+            // if(maxsum>currsum){
+            //     maxsum=maxsum;
+            // }
+            // else{
+            //     maxsum=currsum;
+            // }
+            cout<<currsum<<",";
         }
         cout<<endl;
     }
+    cout<<"Maximum : "<<maxsum;
 }
 int main(){
-  int arr[] = {1,2,3,4,5};
- //  int arr[] = {2,-3,6,-5,4,2};
+    int arr[] = {1,2,3,4,5};
     int n = sizeof(arr)/sizeof(int);
     sumSubArray(arr,n);
 }
