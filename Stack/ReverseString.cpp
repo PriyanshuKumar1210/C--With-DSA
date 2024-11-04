6a01ccc6f0b8fa09c9a3bbe9bb3878c30cc3f0f8
Priyanshu kumar <priyanshukumar23933@gmail.com>
2024-11-04 16:46:40 +0000

    updated

--- Stack/ReverseString.cpp
+++ Stack/ReverseString.cpp
@@ -1,9 +1,31 @@
+/* Time complexity O(n)
+    Space complxity O(n) */
+
 #include<iostream>
 #include<stack>
 #include<string>
 
 using namespace std;
 
+string reverseString(string str){
+    stack<char> s;
+    string ans;
+
+    for(int i=0;i<str.size();i++){
+        s.push(str[i]);
+    }
+
+    while(!s.empty()){
+        char top = s.top();
+        ans += top;
+        s.pop();
+    }
+        return ans;
+}
+
 int main(){
-    
+    string str = "abcd";
+    cout<<"Reverse String : "<<reverseString(str);
+    cout<<endl;
+    return 0;
 }
